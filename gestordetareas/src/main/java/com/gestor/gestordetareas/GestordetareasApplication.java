bd9aa87f138927519846dd2d4e085da0bf304529
jessica orjuela <jedaos12@gmail.com>
2022-02-17 18:09:46 -0500

    creacion proyecto

---
+++ gestordetareas/src/main/java/com/gestor/gestordetareas/GestordetareasApplication.java
@@ -0,0 +1,13 @@
+package com.gestor.gestordetareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestordetareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestordetareasApplication.class, args);
+	}
+
+}
---
+++ gestordetareas/src/test/java/com/gestor/gestordetareas/GestordetareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestor.gestordetareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestordetareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
